67911d09a1b00eede38b40b6f7e95f00b08045a0
Kwaame Ofori-Adjekum <qwaedjekum@gmail.com>
2021-06-10 17:46:59 +0000

    Added sass and nav

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: ["@babel/polyfill", "./src/js/app.js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "./js/[name].[contentHash].bundle.js",
+  },
+  devServer: {
+    contentBase: "./dist",
+    disableHostCheck: true,
+    host: "127.0.0.1",
+    port: "5000",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ["app"],
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        "**/*",
+        "!assets/**",
+        "!img/**",
+        "!fonts/**",
+        "!css/**",
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
